1501b5e8bd952b6cac0f74826ccd97a3dc1ba767
derfrodo <derfrodo0715@gmail.com>
2017-08-08 20:48:50 +0200

    initial commit

---
+++ customBehavior/Behaviours.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace customBehavior
+{
+    /// <summary>
+    /// Credits gehen an https://stackoverflow.com/questions/23316932/invoke-command-when-treeviewitem-is-expanded
+    /// </summary>
+    public static class Behaviours
+    {
+        public static readonly DependencyProperty ExpandingBehaviourProperty =
+            DependencyProperty.RegisterAttached("ExpandingBehaviour",
+                typeof(ICommand),
+                typeof(Behaviours),
+                new PropertyMetadata(OnExpandingBehaviourChanged));
+
+        public static void SetExpandingBehaviour(DependencyObject o, ICommand value)
+        {
+            o.SetValue(ExpandingBehaviourProperty, value);
+        }
+
+        public static ICommand GetExpandingBehaviour(DependencyObject o)
+        {
+            return (ICommand)o.GetValue(ExpandingBehaviourProperty);
+        }
+
+        //Wird bei jeder neuen Zuweisung der Expanding Behaviour Dependency Property zum Objekt "d" aufgerufen.
+        private static void OnExpandingBehaviourChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TreeViewItem tvi = d as TreeViewItem;
+
+            if(tvi != null)
+            {
+                ICommand ic = e.NewValue as ICommand;
+                if(ic != null)
+                {
+                    // Was hier nicht gemacht wird: Wir lösen keinen Handler (falls bereits ein anderes Command auf tvi.Expanded gebunden wurde)!
+                    // Dies ist grundsätzlich nicht möglich in der Lambda-Schreibweise
+                    // Bspw. möglicher Ansatz: konkreter Delegat, welcher in einem Directory vorgehalten wird.
+                    // Allerdings: Unwahrscheinlicher fall, dass 2 Commands gebunden werden.
+                    // Die Zuweisung zu dieser Property fand bislang in meinen Versuchsaufbauten stets innerhalb der XAML-Definition 
+                    // und somit eigentlich nur einmalig (während der Initialisierung) statt
+                    tvi.Expanded += (sender, routedEventArgs) =>
+                    {
+                        if (ic.CanExecute(routedEventArgs))
+                        {
+                            ic.Execute(routedEventArgs);
+                        }
+                        routedEventArgs.Handled = true;
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ customBehavior/ViewModel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace customBehavior
+{
+    public class ViewModel
+    {
+        public int Number { get; set; }
+
+        public List<ItemViewModel> ItemModels { get; set; } = new List<ItemViewModel>
+        {
+            new ItemViewModel {Model = new ItemModel { Message="Some Item" } , Children=new List<ItemViewModel> { new ItemViewModel { Model= new ItemModel { Message = "SubItem" } } } },
+            new ItemViewModel {Model = new ItemModel { Message="Another Item" } }
+        };
+
+    }
+
+    public class ItemViewModel     {
+        public ItemModel Model { get; set; }
+        public List<ItemViewModel> Children { get; set; }
+
+
+        public ICommand ItemExpanding => new RelayCommand<RoutedEventArgs>(e =>
+        {
+            //Hier könnten jetzt auch bspw. children geladen werden...
+             Model.Expanded = "Expanded";
+        });
+    }
+
+    public class ItemModel : INotifyPropertyChanged
+    {
+        private string _message;
+        private string _expanded;
+        public string Message { get { return _message; }
+            set
+            {
+                _message = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Message)));
+            }
+        }
+        public String Expanded {
+            get { return _expanded; }
+            set
+            {
+                _expanded = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Expanded)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    //Implementierung aus: https://stackoverflow.com/questions/22285866/why-relaycommand
+    //Blind für Demonstrationszwecke kopiert.
+    public class RelayCommand<T> : ICommand
+    {
+        #region Fields
+
+        readonly Action<T> _execute = null;
+        readonly Predicate<T> _canExecute = null;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="DelegateCommand{T}"/>.
+        /// </summary>
+        /// <param name="execute">Delegate to execute when Execute is called on the command.  This can be null to just hook up a CanExecute delegate.</param>
+        /// <remarks><seealso cref="CanExecute"/> will always return true.</remarks>
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion
+
+        #region ICommand Members
+
+        ///<summary>
+        ///Defines the method that determines whether the command can execute in its current state.
+        ///</summary>
+        ///<param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        ///<returns>
+        ///true if this command can be executed; otherwise, false.
+        ///</returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        ///<summary>
+        ///Occurs when changes occur that affect whether or not the command should execute.
+        ///</summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        ///<summary>
+        ///Defines the method to be called when the command is invoked.
+        ///</summary>
+        ///<param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to <see langword="null" />.</param>
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        #endregion
+    }
+
+}
