7200580226e50f6a31abb48ca9e445c3843df21d
derfrodo <derfrodo0715@gmail.com>
2017-08-08 21:45:08 +0200

    Anmerkungen

--- customBehavior/Behaviours.cs
+++ customBehavior/Behaviours.cs
@@ -48,12 +48,8 @@ namespace customBehavior
                 
                 if(ic != null)
                 {
-                    // Was hier nicht gemacht wird: Wir lösen keinen Handler (falls bereits ein anderes Command auf tvi.Expanded gebunden wurde)!
-                    // Dies ist grundsätzlich nicht möglich in der Lambda-Schreibweise
-                    // Bspw. möglicher Ansatz: konkreter Delegat, welcher in einem Directory vorgehalten wird.
-                    // Allerdings: Unwahrscheinlicher fall, dass 2 Commands gebunden werden.
-                    // Die Zuweisung zu dieser Property fand bislang in meinen Versuchsaufbauten stets innerhalb der XAML-Definition 
-                    // und somit eigentlich nur einmalig (während der Initialisierung) statt
+                    // Was hier nicht gemacht wird: Wir entfernen keine Zuweisung
+                    // falls bereits ein anderes Command auf tvi.Expanded gebunden wurde.
                     tvi.Expanded += (sender, routedEventArgs) =>
                     {
                         if (ic.CanExecute(routedEventArgs))
